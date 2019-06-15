7c4200842bbaf4ff431e3b859aa619c0d32f67f1
hikarut <ht.hikaru.takahashi@gmail.com>
2019-06-15 15:29:08 +0900

    追加

--- pages/homepage-web/index.vue
+++ pages/homepage-web/index.vue
@@ -1,29 +1,42 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        homepage-web
-      </h1>
-      <h2 class="subtitle">
-        My terrific Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <v-layout row>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-toolbar flat>
+          <v-btn icon>
+            <v-icon>arrow_back</v-icon>
+          </v-btn>
+          <v-toolbar-title>Albums</v-toolbar-title>
+          <v-spacer/>
+          <v-btn icon>
+            <v-icon>search</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-subheader>May</v-subheader>
+        <v-container fluid grid-list-sm>
+          <v-layout row wrap>
+            <v-flex v-for="i in 6" :key="i" xs4>
+              <img :src="`https://randomuser.me/api/portraits/men/${i + 20}.jpg`" class="image" alt="lorem" 
+                   width="100%" 
+                   height="100%">
+            </v-flex>
+          </v-layout>
+        </v-container>
+        <v-subheader>June</v-subheader>
+        <v-container fluid grid-list-sm>
+          <v-layout row wrap>
+            <v-flex v-for="i in 6" :key="i" xs4>
+              <img :src="`https://randomuser.me/api/portraits/women/${i + 5}.jpg`" class="image" alt="lorem" 
+                   width="100%" 
+                   height="100%">
+            </v-flex>
+          </v-layout>
+        </v-container>
+        <v-footer class="mt-5"/>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
-
 <script>
 import Logo from '~/components/Logo.vue'
 
