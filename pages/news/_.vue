f56f1c90c9aa5bc8d81256995cd12640a0c99eb1
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-06-17 16:13:57 +0900

    fix : ニュースページの詳細作成

--- pages/news/_.vue
+++ pages/news/_.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>news</div>
+  <news-wrapper>
+    <template v-slot:title>
+      <h1 class="display-2">{{ displayPost.title.rendered }}</h1>
+    </template>
+
+    <v-flex row>
+      <h4><time>{{ publishDate }}</time>&nbsp;お知らせ</h4>
+    </v-flex>
+
+  </news-wrapper>
 </template>
 
 <script>
 import moment from 'moment'
+import { mapState } from 'vuex'
+import NewsWrapper from '~/components/pages/NewsWrapper'
 
 export default {
+  components: { NewsWrapper },
   async fetch({ store, params, error }) {
     const posts = await store.dispatch('posts/show', {
       postType: 'news',
@@ -17,6 +29,16 @@ export default {
     }
 
     store.commit('posts/setDisplayPost', posts[0])
+  },
+  computed: {
+    publishDate() {
+      return moment(this.displayPost.date).format('YYYY年MM月DD日')
+    },
+    ...mapState('posts', ['displayPost'])
   }
 }
 </script>
+
+<style>
+@import '~/assets/pages/news.scss';
+</style>
