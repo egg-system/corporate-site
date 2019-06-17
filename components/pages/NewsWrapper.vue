bf3e878603a381c6d9df55ffba85e55b3ca010bf
nuno929 <t-mark5@kce.biglobe.ne.jp>
2019-06-17 16:13:45 +0900

    fix : ニュースページの土台作成

---
+++ components/pages/NewsWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout row>
+    <v-flex xs12>
+      <v-layout align-center justify-center class="py-5">
+        <slot name="title"/>
+      </v-layout>
+
+      <v-layout align-center justify-center>
+        <slot/>
+      </v-layout>
+    </v-flex>
+  </v-layout>
+</template>
