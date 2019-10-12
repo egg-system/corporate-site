935e8f39831f8b4eb0d9573eb28aa65230e3c709
hikarut <ht.hikaru.takahashi@gmail.com>
2019-10-12 21:24:16 +0900

    ページの追加

--- pages/about.vue
+++ pages/about.vue
@@ -2,13 +2,7 @@
   <section class="hero is-primary">
     <div class="hero-body">
       <div class="container">
-        <h1 class="title">
-          Egg System
-        </h1>
-        <h2 class="subtitle">
-          x-faCE
-        </h2>
-        <p><nuxt-link to="/about">about</nuxt-link></p>
+        about
       </div>
     </div>
   </section>
