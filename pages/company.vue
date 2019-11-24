d46d0cdd4c7467f06e270b359f8d49fb0bb75083
hikarut <ht.hikaru.takahashi@gmail.com>
2019-11-24 16:37:03 +0900

    地図の追加

--- pages/company.vue
+++ pages/company.vue
@@ -13,6 +13,7 @@
 
       <the-sub-header class="blank" text="会社概要" />
       <the-info />
+      <the-map />
 
       <the-sub-header class="blank" text="代表メッセージ" />
       <the-message class="end" />
@@ -24,6 +25,7 @@
 import TheHeroTitle from '~/components/pages/common/TheHeroTitle.vue'
 import TheSubHeader from '~/components/pages/company/TheSubHeader.vue'
 import TheInfo from '~/components/pages/company/TheInfo.vue'
+import TheMap from '~/components/pages/company/TheMap.vue'
 import TheMission from '~/components/pages/company/TheMission.vue'
 import TheVision from '~/components/pages/company/TheVision.vue'
 import TheValue from '~/components/pages/company/TheValue.vue'
@@ -37,7 +39,8 @@ export default {
     TheMission,
     TheVision,
     TheValue,
-    TheMessage
+    TheMessage,
+    TheMap
   }
 }
 </script>
