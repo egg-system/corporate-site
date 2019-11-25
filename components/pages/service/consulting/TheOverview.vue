636da028499ba18aa79b5ef0a0584272fdbed488
hikarut <ht.hikaru.takahashi@gmail.com>
2019-11-26 08:26:27 +0900

    改行入れたり

--- components/pages/service/consulting/TheOverview.vue
+++ components/pages/service/consulting/TheOverview.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <p>
+    <p class="main-text">
       システム導入およびシステム導入後の運用における<br><b>「計画」「推進」「管理」</b>に沿ったコンサルティングサービスを提供します。
     </p>
-    <p>
-      ①<b>【計画】</b>IT戦略・IT投資計画策定サービス<br>
-      ②<b>【推進】</b>業務改善コンサルティングサービス<br>
-      ③<b>【管理】</b>プロジェクトマネジメントサービス<br>
-    </p>
+    <div class="columns">
+      <div class="column">
+        ①<b>【計画】</b><br>IT戦略・IT投資計画策定サービス
+      </div>
+      <div class="column">
+        ②<b>【推進】</b><br>業務改善コンサルティングサービス
+      </div>
+      <div class="column">
+        ③<b>【管理】</b><br>プロジェクトマネジメントサービス
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,4 +21,10 @@
 </script>
 
 <style lang="scss" scoped>
+.main-text {
+  text-align: center;
+}
+.column {
+  text-align: center;
+}
 </style>
--- pages/service/consulting.vue
+++ pages/service/consulting.vue
@@ -15,7 +15,14 @@
       <the-case />
 
       <div class="contact">
-        <button class="button is-large is-fullwidth" @click="go('/contact')">お問い合わせ</button>
+        <p>
+          サービス内容や事例について等、お気軽にお問い合わせください。<br>
+          依頼するかどうかまだ決まっていない、問い合わせるべきかどうか迷っている、<br>
+          といった状態でもご相談を受け付けております。<br>
+        </p>
+        <div class="contact-button">
+          <button class="button is-large is-fullwidth" @click="go('/contact')">お問い合わせ</button>
+        </div>
       </div>
     </div>
   </div>
@@ -61,6 +68,9 @@ export default {
 .contact {
   padding-top: 30px;
   padding-bottom: 30px;
+  text-align: center;
+}
+.contact-button {
   width: 50%;
   margin: auto;
 }
