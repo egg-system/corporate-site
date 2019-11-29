a7fed0d13a8655cde55975307dde58e59d9ba32f
hikarut <ht.hikaru.takahashi@gmail.com>
2019-11-29 09:09:03 +0900

    事例紹介の調整

--- components/pages/service/consulting/TheCase.vue
+++ components/pages/service/consulting/TheCase.vue
@@ -1,38 +1,96 @@
 <template>
   <div>
-    <div>
-      <h4 class="is-6">①【計画】IT戦略・IT投資計画策定サービス</h4>
-      ◯ 通信教育運営会社におけるシステム化中期計画策定<br>
-      【企業規模：社員数　約200名、参考費用：140万円／月】<br>
-      現行システムおよび現行業務を可視化した上で、問題点を洗い出し、対応方針を策定しました。<br>
-      また、ToBe（理想形）およびCanBe（現実的解）のシステムグランドデザインを作成しました。<br>
-      また、現状の予算を明らかにした上で、今後5年間で発生するシステム投資額についても提示いたしました。<br><br>
+    <h4 class="is-6">①【計画】IT戦略・IT投資計画策定サービス</h4>
+    <div class="columns">
+      <div class="column is-two-fifths">
+        <figure class="image">
+          <img src="https://eggsystem.co.jp/wp-content/uploads/2019/01/sysconsul-case01.jpeg">
+        </figure>
+      </div>
+      <div class="column">
+        <div class="example">
+          <p>
+            <b>通信教育運営会社におけるシステム化中期計画策定</b>
+          </p>
+          <p class="small">
+            <b>【企業規模：社員数　約200名、参考費用：140万円／月】</b><br>
+            現行システムおよび現行業務を可視化した上で、問題点を洗い出し、対応方針を策定しました。<br>
+            また、ToBe（理想形）およびCanBe（現実的解）のシステムグランドデザインを作成しました。<br>
+            また、現状の予算を明らかにした上で、今後5年間で発生するシステム投資額についても提示いたしました。
+          </p>
+        </div>
+      </div>
     </div>
-    <div>
-      <h4 class="is-6">②【推進】業務改善コンサルティングサービス</h4>
-      ◯ 研修事業運営会社における業務改善支援コンサルティング<br>
-      【企業規模：社員数 約200名、参考費用：140万円／月】<br>
-      現行業務の問題点を踏まえたうえで、バックヤード業務（営業支援、販売管理等）の新業務フローを策定いたしました。<br><br>
 
-      ◯ ネット予約システムの導入コンサルティング<br>
-      【エステサロン／パーソナルトレーニングジム、参考費用：月額3万円〜】<br>
-      ネット予約システムの導入により、予約業務の効率化を行いました。<br><br>
+    <h4 class="is-6">②【推進】業務改善コンサルティングサービス</h4>
+    <div class="columns">
+      <div class="column is-two-fifths">
+        <figure class="image">
+          <img src="https://eggsystem.co.jp/wp-content/uploads/2019/01/sysconsul-case02-1024x666.jpeg">
+        </figure>
+      </div>
+      <div class="column">
+        <div class="example">
+          <p>
+            <b>研修事業運営会社における業務改善支援コンサルティング</b>
+          </p>
+          <p class="small">
+            <b>【企業規模：社員数 約200名、参考費用：140万円／月】</b><br>
+            現行業務の問題点を踏まえたうえで、バックヤード業務（営業支援、販売管理等）の新業務フローを策定いたしました。
+          </p>
+        </div>
+        <div class="example">
+          <p>
+            <b>ネット予約システムの導入コンサルティング</b>
+          </p>
+          <p class="small">
+            <b>【エステサロン／パーソナルトレーニングジム、参考費用：月額3万円〜】</b><br>
+            ネット予約システムの導入により、予約業務の効率化を行いました。
+          </p>
+        </div>
+      </div>
     </div>
-    <div>
-      <h4 class="is-6">③【管理】プロジェクトマネジメントサービス</h4>
-      ◯ 整体業におけるPOSシステムおよび基幹システム刷新のプロジェクトマネジメント<br>
-      【企業規模：社員数 約1,500名、参考費用：140万円／月】<br>
-      POSシステムおよび基幹系システム導入プロジェクトにおいてプロジェクトマネジメントを行いました。<br>
-      進捗管理・課題管理・予算管理だけでなく、お客様の立場でのベンダーマネジメントも実施しました。<br><br>
 
-      ◯ 基幹系システム導入プロジェクトにおけるプロジェクトマネジメント<br>
-      【企業規模：社員数 約200名、参考費用：140万円／月】<br>
-      PM支援として、課題管理、予算管理、進捗管理、などを行いました。<br><br>
 
-      ◯ バックヤード業務の改善プロジェクトマネジメント<br>
-      【企業規模：社員数 約1,700名、参考費用：100万円／月】<br>
-      課題管理、予算管理、進捗管理、ユーザー部門への教育などを行いました。<br>
+    <h4 class="is-6">③【管理】プロジェクトマネジメントサービス</h4>
+    <div class="columns">
+      <div class="column is-two-fifths">
+        <figure class="image">
+          <img src="https://eggsystem.co.jp/wp-content/uploads/2019/01/sysconsul-case03-min-1024x629.jpg">
+        </figure>
+      </div>
+      <div class="column">
+        <div class="example">
+          <p>
+            <b>整体業におけるPOSシステムおよび基幹システム刷新のプロジェクトマネジメント</b>
+          </p>
+          <p class="small">
+            <b>【企業規模：社員数 約1,500名、参考費用：140万円／月】</b><br>
+            POSシステムおよび基幹系システム導入プロジェクトにおいてプロジェクトマネジメントを行いました。<br>
+            進捗管理・課題管理・予算管理だけでなく、お客様の立場でのベンダーマネジメントも実施しました。
+          </p>
+        </div>
+        <div class="example">
+          <p>
+            <b>基幹系システム導入プロジェクトにおけるプロジェクトマネジメント</b>
+          </p>
+          <p class="small">
+            <b>【企業規模：社員数 約200名、参考費用：140万円／月】</b><br>
+            PM支援として、課題管理、予算管理、進捗管理、などを行いました。
+          </p>
+        </div>
+        <div class="example">
+          <p>
+            <b>バックヤード業務の改善プロジェクトマネジメント</b>
+          </p>
+          <p class="small">
+            <b>【企業規模：社員数 約1,700名、参考費用：100万円／月】</b><br>
+            課題管理、予算管理、進捗管理、ユーザー部門への教育などを行いました。
+          </p>
+        </div>
+      </div>
     </div>
+
   </div>
 </template>
 
@@ -40,4 +98,26 @@
 </script>
 
 <style lang="scss" scoped>
+h4 {
+  text-align: center;
+}
+.image {
+  margin: auto;
+}
+.example {
+  padding: 0.5em 1em;
+  margin: 2em 0;
+  color: $black;
+  background: $whitesmoke;
+  border-radius: 9px;
+  max-width: 800px;
+  margin: 0 auto 10px;
+}
+.example p {
+  margin: 10px;
+  padding: 0;
+}
+.small {
+  font-size: 14px;
+}
 </style>
