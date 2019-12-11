7ece6bbd2332e5e676ce9eab05b1c1c7288262ea
hikarut <ht.hikaru.takahashi@gmail.com>
2019-12-11 09:13:07 +0900

    システム開発ページ

---
+++ components/pages/service/development/TheOverview.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="columns">
+    <div class="column center">
+      <p class="text">
+        作ることがゴールではなく<br><b>お客様のニーズにあったシステム開発を行います</b><br><br>
+        ①ホームページ制作<br>
+        ②Webシステム開発
+      </p>
+    </div>
+    <div class="column is-two-fifths">
+      <figure class="image">
+        <img src="https://eggsystem.co.jp/image/btn_homepage.jpg">
+      </figure>
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+// PC版
+@media screen and (min-width: 769px) {
+  .text {
+    font-size: 20px;
+  }
+}
+.center {
+  margin: auto;
+}
+</style>
