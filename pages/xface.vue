34137b8a73aa894422392401196ca8279c91be9c
hikarut <ht.hikaru.takahashi@gmail.com>
2019-12-23 13:05:06 +0900

    画像追加

--- pages/xface.vue
+++ pages/xface.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
     <the-hero-title main-text="x-face" />
+    <div class="overview">
+      <the-overview />
+    </div>
   </div>
 </template>
 
 <script>
 import TheHeroTitle from '~/components/pages/common/TheHeroTitle.vue'
+import TheOverview from '~/components/pages/service/xface/TheOverview.vue'
 
 export default {
   components: {
-    TheHeroTitle
+    TheHeroTitle,
+    TheOverview
   }
 }
 </script>
