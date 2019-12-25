89f6554844d0a01b9cc7fab7d492ef9979a05f18
hikarut <ht.hikaru.takahashi@gmail.com>
2019-12-26 07:55:27 +0900

    メンバー紹介部分修正

--- components/pages/service/xface/TheMember.vue
+++ components/pages/service/xface/TheMember.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="columns">
     <div class="column is-one-third">
-      <figure class="image is-128x128">
-        <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+      <figure class="image">
+        <img class="is-rounded" src="https://eggsystem.co.jp/wp-content/uploads/2019/05/201905-tsubasa02-1024x1024.jpg">
       </figure>
+      <div>
+        代表取締役　CEO<br>
+        <b>高橋 翼</b>  TSUBASA TAKAHASHI<br>
+
+        「献身的な人のためにITシステムを活用する」という理念を元に、株式会社エッグシステムを創業。強みは真面目さだけ。
+      </div>
     </div>
     <div class="column is-one-third">
-      <figure class="image is-128x128">
-        <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+      <figure class="image">
+        <img class="is-rounded" src="https://eggsystem.co.jp/wp-content/uploads/2019/05/hikaru01-min-1024x1024.jpg">
       </figure>
+      <div>
+        CTO<br>
+        <b>高橋 光</b>  HIKARU TAKAHASHI<br>
+
+        エッグシステム社員。ITエンジニア／マネージャー。複業社員としてWebサイト開発やセミナー講師などを行う。
+      </div>
     </div>
     <div class="column is-one-third">
-      <figure class="image is-128x128">
-        <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+      <figure class="image">
+        <img class="is-rounded" src="https://eggsystem.co.jp/wp-content/uploads/2019/05/nunomura01-min-1024x1024.jpg">
       </figure>
+      <div>
+        システムエンジニア<br>
+        <b>布村 武大</b>  TAKEHIRO NUNOMURA<br>
+
+        エッグシステム社員。さすらいのシステムエンジニア。
+      </div>
     </div>
   </div>
 </template>
@@ -22,4 +40,11 @@
 </script>
 
 <style lang="scss" scoped>
+b {
+  font-size: 22px;
+}
+.image {
+  margin: auto;
+  width: 200px;
+}
 </style>
