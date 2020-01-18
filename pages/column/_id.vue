dc874e355e52f3b6655f9cfcb180ea3dc457d6c9
hikarut <ht.hikaru.takahashi@gmail.com>
2020-01-18 13:11:44 +0900

    コラムの詳細ページ作成

---
+++ pages/column/_id.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <the-hero-title :main-text="column.title" />
+    <div class="column-item">
+      <figure class="image">
+        <img :src="column.img">
+      </figure>
+      <div class="sub">
+        <span class="date">{{ column.date }}</span>
+        <span class="tag is-warning">
+          {{ column.label }}
+        </span>
+      </div>
+      <div class="main">
+        <div v-html="column.content" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheHeroTitle from '~/components/pages/common/TheHeroTitle.vue'
+
+export default {
+  components: {
+    TheHeroTitle
+  },
+  data: () => ({
+    column: {
+      title:
+        'デジタルトランスフォーメーション（DX）とは？DXレポートから中小企業での活用方法まで解説（前編）',
+      date: '2019/11/03',
+      label: 'info',
+      url: '/about',
+      img:
+        'https://eggsystem.co.jp/wp-content/uploads/2019/10/201910-dx3-1024x682.jpg',
+      content: '<div>aaaa</div><div>bbbbb</div><div>ああああ</div>'
+    }
+  }),
+  async asyncData({ params }) {
+    // idからデータを取得
+    return {
+      id: params.id
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.column-item {
+  width: 70%;
+  margin: 0 auto;
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+// スマホ版
+@media screen and (max-width: 400px) {
+  .column-item {
+    width: 90%;
+  }
+}
+.sub {
+  margin-top: 20px;
+}
+.date {
+  font-size: 20px;
+}
+.tag {
+  font-size: 14px;
+}
+.date {
+  margin-right: 20px;
+}
+.main {
+  margin-top: 20px;
+  font-size: 24px;
+}
+</style>
