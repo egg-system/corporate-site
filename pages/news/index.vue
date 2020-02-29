c6357143237471b98966655bdd0c8978690a7719
AZUMA YUKI <azm0717@gmail.com>
2020-03-01 00:44:00 +0900

    add：asyncを使用した外部APIからのデータ取得部分を実装

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -10,73 +10,40 @@
 <script>
 import TheHeroTitle from '~/components/pages/common/TheHeroTitle.vue'
 import TheNewsList from '~/components/pages/common/TheNewsList.vue'
+import axios from 'axios'
+const PAGE_NAME = 'news'
+const API_VERSION = 'v1'
+const microCmsUrl = [
+  process.env.eggSystemApiDomain,
+  API_VERSION,
+  PAGE_NAME
+].join('/')
 
 export default {
   components: {
     TheHeroTitle,
     TheNewsList
   },
-  data: () => ({
-    news: [
-      {
-        title: '本社移転のお知らせ',
-        date: '2019/06/03',
-        label: 'info',
-        url: '/news/aaa'
-      },
-      {
-        title: 'H30年度（2019年度） IT導入支援事業者として登録されました',
-        date: '2019/04/26',
-        label: 'info',
-        url: '/news/aaa'
-      },
-      {
-        title: '近代中小企業（KinChu）2019年4月号 掲載のお知らせ',
-        date: '2019/04/03',
-        label: 'info',
-        url: '/news/aaa'
-      },
-      {
-        title: '《2019年新春》本年もよろしくお願い致します',
-        date: '2019/01/07',
-        label: 'info',
-        url: '/news/aaa'
-      },
-      {
-        title:
-          'H29年度IT導入補助金の採択状況、【三次公募、第一回】弊社申請分は全て交付決定いたしました',
-        date: '2018/10/09',
-        label: 'info',
-        url: '/news/aaa'
-      },
-      {
-        title:
-          'H29年度IT導入補助金【二次公募】弊社申請分は全て交付決定いたしました',
-        date: '2018/08/18',
-        label: 'info',
-        url: '/news/aaa'
-      },
-      {
-        title:
-          'H29年度IT導入補助金【一次公募】弊社申請分は全て交付決定いたしました',
-        date: '2018/06/15',
-        label: 'info',
-        url: '/news/aaa'
-      },
-      {
-        title: 'ホームページをリニューアルいたしました',
-        date: '2018/06/04',
-        label: 'info',
-        url: '/news/aaa'
-      },
-      {
-        title: 'H29年度 IT導入支援事業者として登録されました',
-        date: '2018/05/11',
-        label: 'info',
-        url: '/news/aaa'
-      }
-    ]
-  })
+  data() {
+    return {
+      news: ''
+    }
+  },
+  head: {
+    script: []
+  },
+  async asyncData() {
+    const { data } = await axios
+      .get(microCmsUrl, {
+        headers: { 'X-API-kEY': process.env.microCmsApiKey }
+      })
+      .catch(err => {
+        return err.response
+      })
+    return {
+      news: data.contents
+    }
+  }
 }
 </script>
 
