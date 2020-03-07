3cf28d4d375f4364026c2b9156026687d4cd5a07
AZUMA YUKI <azm0717@gmail.com>
2020-03-07 18:40:16 +0900

    add：Module not found: Error: Can’t resolve ‘fs’のエラー対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,6 +62,9 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {
+      config.node = {
+        fs: 'empty'
+      }
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
