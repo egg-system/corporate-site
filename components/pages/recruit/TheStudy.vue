56313c45328cb84ea30f8d15561bf1359d4fe4b7
tada <yuuya.tada@risata181.com>
2020-03-11 03:32:54 +0900

    勉強会component作成

--- components/pages/recruit/TheStudy.vue
+++ components/pages/recruit/TheStudy.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="study">
-    <p class="top-message">
+    <p class="top-message is-size-3">
       システムエンジニア向け勉強会
     </p>
-    <p class="sub-message">
-      システムエンジニア向け勉強会を定期的に開催しています。
-      
-    </p>
-    <div class="contact-button">
-      <button class="button is-black is-large is-fullwidth" @click="go('/contact')">
-        詳しく見る&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;＞
-      </button>
+    <div class="columns">
+      <figure class="column img">
+        <img src="https://eggsystem.co.jp/wp-content/uploads/2019/05/20190518-member01-1024x684.jpg">
+      </figure>
+      <section class="column">
+        <p class="sub-message">
+          システムエンジニア向け勉強会を定期的に開催しています。
+          弊社の取り組みにご興味のある方だけでなく、スキルアップとしてもご参加いただければ幸いです。
+        </p>
+        <div class="contact-button">
+          <button class="button is-black is-large is-fullwidth" @click="go('/contact')">
+            conpas(外部サイト)へ&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;＞
+          </button>
+          <button class="button is-black is-large is-fullwidth" @click="go('/contact')">
+            TECH PLAY(外部サイト)へ&nbsp;&nbsp;&nbsp;
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;＞
+          </button>
+        </div>
+      </section>
     </div>
   </div>
 </template>
--- pages/recruit.vue
+++ pages/recruit.vue
@@ -14,23 +14,32 @@
       <the-contact />
     </div>
     <the-about-us />
+    <the-study />
+    <the-sub-header class="blank" text="最近の活動・コラム" />
+    <the-column class="end" />
   </div>
 </template>
 
 <script>
 import TheHeroTitle from '~/components/pages/common/TheHeroTitle.vue'
+import TheSubHeader from '~/components/pages/common/TheSubHeader.vue'
+import TheContact from '~/components/pages/common/TheContact.vue'
 import TheMainImage from '~/components/pages/recruit/TheMainImage.vue'
 import TheMainMessage from '~/components/pages/recruit/TheMainMessage.vue'
 import TheAboutUs from '~/components/pages/recruit/TheAboutUs.vue'
-import TheContact from '~/components/pages/common/TheContact.vue'
+import TheStudy from '~/components/pages/recruit/TheStudy.vue'
+import TheColumn from '~/components/pages/about/TheColumn.vue'
 
 export default {
   components: {
     TheHeroTitle,
+    TheSubHeader,
+    TheContact,
     TheMainImage,
     TheMainMessage,
-    TheAboutUs,
-    TheContact
+    TheColumn,
+    TheStudy,
+    TheAboutUs
   }
 }
 </script>
@@ -40,5 +49,18 @@ export default {
   width: 50%;
   margin: 30px auto 30px;
   text-align: center;
+}
+.blank {
+  margin-top: 70px;
+  margin-bottom: 30px;
+}
+.end {
+  margin: 0 0 50px;
+}
+@media screen and (max-width: 600px) {
+  .end {
+    margin: 0;
+    padding-bottom: 0;
+  }
 }
 </style>
