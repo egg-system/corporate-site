2968a69ea1067b312f570420683cbeb07ce4b3b3
tada <yuuya.tada@risata181.com>
2020-03-15 00:41:18 +0900

    私たちについて修正

--- components/pages/recruit/TheAboutUs.vue
+++ components/pages/recruit/TheAboutUs.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="aboutus has-background-light columns">
+  <div class="aboutus has-background-light columns is-vcentered">
     <section class="column">
       <p class="top-message is-size-3">
         わたしたちについて
@@ -9,15 +9,14 @@
         一杯のスープに象徴される「豊かな食事」を提案し続けることで、
         人々の生活を豊かで幸せなものにしたいと考えています。
       </p>
-      <div class="contact-button">
-        <button class="button is-black is-large is-fullwidth" @click="go('/contact')">
-          詳しく見る&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;＞
+      <div class="contact-button has-text-right">
+        <button class="button is-light" @click="go('/contact')">
+          詳しく見る→
         </button>
       </div>
     </section>
-    <figure class="column img">
-      <img src="~/assets/members/tsubasa02.jpg">
+    <figure class="column image">
+      <img src="https://eggsystem.co.jp/wp-content/uploads/2019/05/20190518-member01-1024x684.jpg">
     </figure>
   </div>
 </template>
@@ -36,5 +35,11 @@ export default {
     display: -webkit-flex;
     display: flex;
   }
+}
+.top-message {
+  margin-bottom: 20px;
+}
+.about-us {
+  padding: 30px;
 }
 </style>
--- components/pages/recruit/TheStudy.vue
+++ components/pages/recruit/TheStudy.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="study">
-    <p class="top-message is-size-3">
-      システムエンジニア向け勉強会
-    </p>
-    <div class="columns">
-      <figure class="column img">
+    <div class="columns is-marginless">
+      <figure class="column image has-text-centered">
+        <p class="top-message is-size-3 has-text-centered">
+          システムエンジニア向け勉強会
+        </p>
         <img src="https://eggsystem.co.jp/wp-content/uploads/2019/05/20190518-member01-1024x684.jpg">
       </figure>
       <section class="column">
         <p class="sub-message">
-          システムエンジニア向け勉強会を定期的に開催しています。
+          システムエンジニア向け勉強会を定期的に開催しています。<br><br>
           弊社の取り組みにご興味のある方だけでなく、スキルアップとしてもご参加いただければ幸いです。
         </p>
         <div class="contact-button">
-          <button class="button is-black is-large is-fullwidth" @click="go('/contact')">
-            conpas(外部サイト)へ&nbsp;&nbsp;&nbsp;
+          <button class="button is-black is-medium" @click="go('/contact')">
+            conpas(外部サイト)へ&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;＞
           </button>
-          <button class="button is-black is-large is-fullwidth" @click="go('/contact')">
-            TECH PLAY(外部サイト)へ&nbsp;&nbsp;&nbsp;
+          <button class="button is-black is-medium is-fullwidth" @click="go('/contact')">
+            TECH PLAY(外部サイト)へ
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;＞
           </button>
         </div>
@@ -36,4 +36,14 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.top-message {
+  margin-bottom: 40px;
+}
+.image img {
+  margin: 0 auto;
+}
+.contact-button {
+  position: absolute;
+  bottom: 0;
+}
 </style>
