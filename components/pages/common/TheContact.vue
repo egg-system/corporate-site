e3c392c0bff2d8f79467f5122d3b2d8aae91635f
tada <yuuya.tada@risata181.com>
2020-03-22 10:02:13 +0900

    採用ボタンの幅調整

--- components/pages/common/TheContact.vue
+++ components/pages/common/TheContact.vue
@@ -1,33 +1,58 @@
 <template>
   <div class="contact">
-    <div class="contact-button">
-      <button class="button has-text-white has-background-black is-large is-fullwidth" @click="go('/contact')">採用について質問する<span class="right_arrow">＞</span></button>
+    <div class="contact-columns">
+      <nuxt-link to="/contact">
+        <div class="contact-column">
+          <p>採用について質問する<span>></span></p>
+        </div>
+      </nuxt-link>
     </div>
   </div>
 </template>
 
 <script>
-import method from '~/mixins/method'
-
-export default {
-  mixins: [method]
-}
 </script>
 
 <style lang="scss" scoped>
 .contact {
-  padding-top: 30px;
-  padding-bottom: 30px;
+  width: 80%;
+  margin: 0 auto;
+  margin-top: 8%;
+  margin-bottom: 10%;
+  background: $white;
+}
+.contact-columns {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.contact-columns a {
+  width: 60%;
+}
+.contact-column {
+  padding: 0;
+  height: 50px;
   text-align: center;
+  line-height: 50px;
+  font-size: 15px;
+  font-weight: bold;
+  background: $darkblack;
+  border-radius: 4px;
 }
-.contact-button {
-  width: 50%;
-  margin: auto;
+.contact-column p {
+  color: $lightgray;
+  font-size: 18px;
 }
-// PC版
-@media screen and (min-width: 769px) {
-  .right_arrow {
-    margin-left: 250px;
+.contact-column p span {
+  margin-left: 15px;
+}
+@media screen and (max-width: 600px) {
+  .contact-columns {
+    display: block;
+  }
+  .contact-column {
+    width: 100%;
+    margin: 15px auto;
   }
 }
 </style>
