91ccbe8ee80d93a4275af8650f364171a6b446c6
gentlable <35590706+gentlable@users.noreply.github.com>
2020-03-26 03:34:14 +0900

    画像差し替え、文言入力
    画像差し替え、文言入力
    実装方法変更

--- pages/request.vue
+++ pages/request.vue
@@ -1,48 +1,40 @@
 <template>
   <div>
     <the-hero-title main-text="ご依頼について" />
-    <div class="content">
-      <table class="table">
-        <tbody>
-          <tr class="darksmoke-underline">
-            <the-about />
-          </tr>
-          <tr class="darksmoke-underline">
-            <the-sub-header :is-line="true" text="ご依頼の流れ" />
-            <the-flow />
-          </tr>
-          <tr class="darksmoke-underline">
-            <the-sub-header :is-line="true" text="当社サービスについて" />
-            <the-service :services="services" class="service"/>
-          </tr>
-          <tr class="darksmoke-underline">
-            <the-message />
-          </tr>
-          <tr>
-            <the-sub-header :is-line="true" text="最近の活動・コラム" />
-            <the-column />
-          </tr>
-        </tbody>
-      </table>
+    <div class="content  page-content">
+      <div class="message lightgray-underline">
+        <p>ご要件やご予算が決まっていない段階でのご相談でも問題ありません。</p>
+        <br>
+        <p>「こんな相談をしてよいのかどうか分からないけど・・・」</p>
+        <p>「まずは調査と分析だけ依頼したい」</p>
+        <p>「このくらいのシステムを作る場合の費用を知りたい」</p>
+        <br>
+        <p>など、どんな内容もまずはお気軽にご相談ください。</p>
+      </div>
+      <the-sub-header :is-line="true" text="ご依頼の流れ" class="subheader" />
+      <the-flow class="lightgray-underline" />
+      <the-sub-header :is-line="true" text="当社サービスについて" class="subheader" />
+      <the-service :services="services" class="service lightgray-underline"/>
+      <the-message />
+      <!-- <the-sub-header :is-line="true" text="最近の活動・コラム" class="subheader" />
+      <the-column /> -->
     </div>
   </div>
 </template>
 
 <script>
-import TheHeroTitle from '~/components/pages/common/TheHeroTitle.vue'
+import TheHeroTitle from '~/components/pages/request/TheHeroTitle.vue'
 import TheSubHeader from '~/components/pages/request/TheSubHeader.vue'
-import TheAbout from '~/components/pages/request/TheAbout.vue'
 import TheFlow from '~/components/pages/request/TheFlow.vue'
 import TheService from '~/components/pages/request/TheService.vue'
 import TheMessage from '~/components/pages/request/TheMessage.vue'
 import TheColumn from '~/components/pages/request/TheColumn.vue'
-import services from '~/assets/js/services.js'
+import services from '~/assets/request/js/services.js'
 
 export default {
   components: {
     TheHeroTitle,
     TheSubHeader,
-    TheAbout,
     TheFlow,
     TheService,
     TheMessage,
@@ -55,9 +47,12 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.content {
-  width: 80%;
-  margin: 0 auto;
+.message {
+  background-color: inherit;
+  padding-bottom: 4rem;
+}
+.subheader {
+  margin-top: 1rem;
 }
 .link {
   text-align: right;
