1b4142e73e8c634faac52d964d8ffdfab71f0311
tada <yuuya.tada@risata181.com>
2020-03-29 21:26:30 +0900

    お問い合わせ わたしたちについてmetaタグ作成

--- pages/about.vue
+++ pages/about.vue
@@ -27,6 +27,52 @@ export default {
     TheValue,
     TheMessage,
     TheColumn
+  },
+  head() {
+    return {
+      title: 'わたしたちについて',
+      meta: [
+        {
+          hid: 'about-description',
+          name: 'description',
+          content:
+            '株式会社エッグシステムでは、《「目の前の人に喜んでもらうこと」を目的とし行動する人が報われる社会》の実現のために、ITシステムを活用します。'
+        },
+        {
+          hid: 'about-keywords',
+          name: 'keywords',
+          content:
+            '献身的な人のためにITシステムを活用する, 100人のコンサルティングエンジニア集団をつくる, 圧倒的当事者意識'
+        },
+        {
+          hid: 'about-twitter:card',
+          property: 'twitter:card',
+          content: 'summary'
+        },
+        { hid: 'about-og:type', property: 'og:type', content: 'article' },
+        {
+          hid: 'about-og:title',
+          property: 'og:title',
+          content: 'わたしたちについて'
+        },
+        {
+          hid: 'about-og:description',
+          property: 'og:description',
+          content:
+            '株式会社エッグシステムでは、《「目の前の人に喜んでもらうこと」を目的とし行動する人が報われる社会》の実現のために、ITシステムを活用します。'
+        },
+        {
+          hid: 'about-og:url',
+          property: 'og:url',
+          content: 'https://eggsystem.co.jp/about'
+        },
+        {
+          hid: 'about-og:image',
+          property: 'og:image',
+          content: 'https://eggsystem.co.jp/ogp/top-about.JPG'
+        }
+      ]
+    }
   }
 }
 </script>
