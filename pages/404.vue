9e3b700f9025b958867f1b196a613b884ae56d1e
hikarut <ht.hikaru.takahashi@gmail.com>
2020-03-30 21:39:49 +0900

    404ページの作成。リダイレクトの追加

---
+++ pages/404.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="hero is-medium">
+    <div class="hero-body">
+      <div class="container center-text">
+        <h1>
+          指定されたページが見つかりませんでした。
+        </h1>
+        <nuxt-link to="/">トップページ</nuxt-link>に戻る。
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'エラー'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
