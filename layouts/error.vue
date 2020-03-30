8c0a09b4e2203e0a588b4c06ed08d930412f5c94
hikarut <ht.hikaru.takahashi@gmail.com>
2020-03-30 23:02:52 +0900

    エラーページ作成

---
+++ layouts/error.vue
@@ -0,0 +1,29 @@
+<template>
+  <section class="hero is-medium">
+    <div class="hero-body">
+      <div class="container center-text">
+        <h1 v-if="statusCode === 404">指定されたページが見つかりませんでした。</h1>
+        <h1 v-else >エラーです。</h1>
+        <nuxt-link to="/">トップページ</nuxt-link>に戻る。
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'エラー'
+    }
+  },
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
