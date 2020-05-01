5706a47a32baeac17e367c41eb98f5a7430577bd
tada <phobia4242@gmail.com>
2020-05-01 23:32:12 +0900

    ニュース一覧デザイン仮

--- components/pages/common/TheNewsList.vue
+++ components/pages/common/TheNewsList.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="news-list">
-    <li>
-      <div v-for="(item, i) in news" :key="i">
-        <ul>
-          <div class="columns">
-            <div class="columns is-mobile">
-              <div class="column news-date is-4-mobile">{{ item.display_at | dayjs }}</div>
-              <div class="column is-4-mobile">
-                <span class="tag is-primary">
-                  {{ item.label.label }}
-                </span>
-              </div>
-            </div>
-
+    <div class="columns is-multiline">
+      <ul>
+        <div v-for="(item, i) in news" :key="i" :class="[ 
+          i <= 2 ? 'column is-one-third border'+i : 'column is-one-quarter border'+i,
+        ]" >
+          <li class="news-inner">
             <div class="news-title">
               <nuxt-link :to="'/news/' + item.id">
                 {{ item.title }}
               </nuxt-link>
             </div>
-          </div>
-        </ul>
-      </div>
-    </li>
+            <div class="column news-date is-4-mobile">{{ item.display_at | dayjs }}</div>
+            <div class="column is-4-mobile">
+              <span class="tag info-color">
+                {{ item.label.label }}
+              </span>
+            </div>
+          </li>
+        </div>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -46,14 +45,51 @@ export default {
 }
 .news-title {
   padding: 5px 10px;
+  height: 100px;
+}
+.news-title a {
+  color: $black;
 }
 .news-date {
   text-align: right;
   font-size: 14px;
-  color: $darksmoke;
+  // color: $darksmoke;
+  color: $black;
 }
-li {
-  list-style: none;
+.is-one-third {
+  display: inline-block;
+  border-right: 1px solid $lightgray;
+  border-left: 1px solid $lightgray;
+  margin: 10px 0;
+}
+.is-one-quarter {
+  display: inline-block;
+  border-right: 1px solid $lightgray;
+  border-left: 1px solid $lightgray;
+  margin: 10px 0;
+}
+.info-color {
+  color: $white;
+  background-color: $black;
+}
+.border0,
+.border3,
+.border7,
+.border11,
+.border15,
+.border19 {
+  border-left: none;
+}
+.border2,
+.border6,
+.border10,
+.border14,
+.border18,
+.border22 {
+  border-right: none;
+}
+ul li {
+  display: inline-block;
 }
 @media screen and (max-width: 600px) {
   .news-list {
