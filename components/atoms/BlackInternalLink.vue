e9b9f4be7fb9585ae786b85d1420f65882a9819c
gentlable <35590706+gentlable@users.noreply.github.com>
2020-05-09 03:23:55 +0900

    黒色ボタンコンポーネント化
    アトミックデザインを参考にコンポーネント化に切り分けました。

---
+++ components/atoms/BlackInternalLink.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="link">
+    <nuxt-link :to="to" class="button is-dark">
+      <span class="link-message">{{ label }}</span>
+      <span class="column-arrow" />
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    to: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
