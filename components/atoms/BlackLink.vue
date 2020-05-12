e6edc4591517c155e09f1c346083df66cc9a3ad2
gentlable <35590706+gentlable@users.noreply.github.com>
2020-05-13 01:08:25 +0900

    外部リンクと内部リンク統一

---
+++ components/atoms/BlackLink.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="link">
+    <a v-if="external" :href="href" target="_blank" class="button is-dark">
+      <span class="link-message">{{ label }}</span>
+      <span class="column-arrow" />
+    </a>
+    <nuxt-link v-if="internal" :to="to" class="button is-dark">
+      <span class="link-message">{{ label }}</span>
+      <span class="column-arrow" />
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    href: {
+      type: String,
+      default: ''
+    },
+    to: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    external() {
+      return this.href != ''
+    },
+    internal() {
+      return this.href == ''
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
