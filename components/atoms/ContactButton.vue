4c9372edc8953ddf46186417541aaaac2a27dc8c
Tsubasa <38577320+eggsystem@users.noreply.github.com>
2020-08-05 15:59:44 +0900

    文言変更

--- components/atoms/ContactButton.vue
+++ components/atoms/ContactButton.vue
@@ -2,7 +2,7 @@
   <div class="width-7 center">
     <nuxt-link to="/contact" >
       <div class="center padding-1 font-150 font-bold color-white border-radius bg-lightbabyblue">
-        ご相談はこちらで承ります
+        ご相談はこちらから
       </div>
     </nuxt-link>
   </div>
