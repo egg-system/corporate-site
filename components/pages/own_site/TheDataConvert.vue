955ceb7f7cfa2cc4072d7b38defaf9ba15317844
tasdsn <phobia4242@gmail.com>
2020-08-10 11:52:07 +0900

    自社サイトページ概要

---
+++ components/pages/own_site/TheDataConvert.vue
@@ -0,0 +1,149 @@
+<template>
+  <div>
+    <the-sub-header text="「Data Convert」サービス" class="subheader" />
+    <div class="columns reverse">
+      <div class="column is-half">
+        <figure class="image">
+          <img :src="dataconvertImage">
+        </figure>
+      </div>
+      <div class="column is-half system-consulting">
+        <div class="message">Webブラウザ上でcsvデータを簡単に変換できるサービスです。</div>
+        <div class="message">
+          HTTPS通信を使っているため安全に通信を行うことができ、csv変換後1時間以内でデータを強制削除するのでアップロードした
+          データがサーバー上に残り続けることがなく安心にお使いいただけます。
+        </div>
+        <div class="link">
+          <nuxt-link v-scroll-to="'.dataconvert-use'" to class="button is-dark">
+            <span class="link-message">利用方法へ</span>
+            <span class="column-arrow" />
+          </nuxt-link>
+        </div>
+      </div>
+    </div>
+    <h3 class="dataconvert-title">「Data Convert」サービス利用方法</h3>
+    <div class="dataconvert-use">
+      <ul>
+        <li>変換したいcsvファイルを選択する</li>
+        <li>csvの変換設定を入力する</li>
+        <li>変換したCSVファイルをダウンロードする</li>
+      </ul>
+    </div>
+    <div class="link">
+      <a class="button is-dark" target="_blank" href="https://convert-service.x-face.net">
+        「Data Convert」サービスはこちらから
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheSubHeader from '~/components/pages/service/common/TheSubHeader.vue'
+import TheCase from '~/components/pages/service/common/TheCase.vue'
+import dataconvertImage from '~/assets/own_site/dataconvert-screen.png'
+import systemConsultingCases from '~/assets/service/js/SystemConsultingCases.js'
+
+export default {
+  components: {
+    TheSubHeader,
+    TheCase
+  },
+  data: () => ({
+    dataconvertImage,
+    cases: systemConsultingCases.cases
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  letter-spacing: 1px;
+}
+.columns {
+  margin: auto;
+  margin-top: 1rem;
+  .is-half {
+    padding: 2rem;
+    padding-top: 1rem;
+  }
+}
+.message {
+  background-color: inherit;
+  line-height: 1.25 !important;
+}
+.columns {
+  margin-bottom: 3rem;
+  .column {
+    position: relative;
+    div {
+      line-height: 1.75;
+    }
+  }
+}
+.table {
+  border: 1px solid $lightgray;
+  margin: 0px;
+  margin-bottom: 3rem;
+  .column {
+    padding: 0px;
+  }
+}
+.table-title {
+  margin-bottom: 1rem;
+}
+@media screen and (min-width: 769px) {
+  .left::before {
+    position: absolute;
+    content: '';
+    width: 1px;
+    height: 90%;
+    top: 5%;
+    right: 0;
+    background: $lightgray;
+  }
+  .reverse {
+    flex-direction: row-reverse;
+  }
+}
+@media screen and (max-width: 768px) {
+  .duration-cost::after {
+    position: absolute;
+    content: '';
+    width: 90%;
+    height: 1px;
+    background: $lightgray;
+    margin-top: 0.5rem;
+  }
+  .five-ws {
+    margin-bottom: 0.25rem;
+    .description {
+      margin-left: 1rem;
+    }
+  }
+}
+.dataconvert-use {
+  padding-top: 30px;
+}
+.dataconvert-title {
+  text-align: center;
+}
+.dataconvert-use li {
+  font-size: 20px;
+  text-align: center;
+  padding: 10px 5px;
+  list-style: none;
+}
+.link {
+  width: 30rem;
+  margin: 30px auto;
+}
+
+@media screen and (max-width: 600px) {
+  .dataconvert-use p {
+    font-size: 18px;
+  }
+  .link {
+    width: 100%;
+  }
+}
+</style>
---
+++ pages/own_site.vue
@@ -0,0 +1,156 @@
+<template>
+  <div>
+    <div class="content page-content">
+      <the-hero-title main-text="自社サービス紹介" />
+      <div class="columns">
+        <div class="consulting column is-half">
+          <figure class="image">
+            <img :src="dataconvertImage">
+          </figure>
+          <div class="is-size-5">Data Convert</div>
+          <div class="link">
+            <nuxt-link v-scroll-to="'#the-data-convert'" to class="button is-dark">
+              <span class="link-message">詳細へ</span>
+              <span class="column-arrow" />
+            </nuxt-link>
+          </div>
+        </div>
+        <div class="development column is-half">
+          <figure class="image">
+            <img :src="dataconvertImage">
+          </figure>
+          <div class="is-size-5">BorderlessGYM</div>
+          <div class="link">
+            <nuxt-link v-scroll-to="'#the-borderless-gym'" to class="button is-dark">
+              <span class="link-message">詳細へ</span>
+              <span class="column-arrow" />
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+      <the-data-convert id="the-data-convert" />
+      <the-borderless-gym id="the-borderless-gym" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TheHeroTitle from '~/components/pages/common/TheHeroTitle.vue'
+import TheDataConvert from '~/components/pages/own_site/TheDataConvert.vue'
+import TheBorderlessGym from '~/components/pages/own_site/TheBorderlessGym.vue'
+import consultingImage from '~/assets/service/img/service-consult01.JPG'
+import dataconvertImage from '~/assets/own_site/dataconvert-screen.png'
+import TheContact from '~/components/pages/top/TheContact.vue'
+
+export default {
+  components: {
+    TheHeroTitle,
+    TheDataConvert,
+    TheBorderlessGym,
+    TheContact
+  },
+  data: () => ({
+    consultingImage,
+    dataconvertImage
+  }),
+  head() {
+    return {
+      title: 'サービス',
+      meta: [
+        {
+          hid: 'service-description',
+          name: 'description',
+          content:
+            'システム導入や導入後の運用を軸に「計画」「推進」「管理」に沿った3つのシステムコンサルティングサービス（IT戦略/IT投資計画策定、システム化支援/業務改善、プロジェクトマネジメント）を提供しています。また、ご要望に合わせてWebシステムや業務システムの開発を行います。'
+        },
+        {
+          hid: 'service-keywords',
+          name: 'keywords',
+          content:
+            'サービス紹介, システムコンサルティング, 開発, IT戦略, IT投資計画策定, システム化支援, 業務改善, ToBe, CanBe, プロジェクトマネジメント, サロン, 整体, エステ, 接骨院, マッサージ, トレーニングジム, EGG'
+        },
+        {
+          hid: 'service-twitter:card',
+          property: 'twitter:card',
+          content: 'summary'
+        },
+        {
+          hid: 'service-og:type',
+          property: 'og:type',
+          content: 'article'
+        },
+        {
+          hid: 'service-og:title',
+          property: 'og:title',
+          content: 'サービス'
+        },
+        {
+          hid: 'service-og:description',
+          property: 'og:description',
+          content:
+            'システム導入や導入後の運用を軸に「計画」「推進」「管理」に沿った3つのシステムコンサルティングサービス（IT戦略/IT投資計画策定、システム化支援/業務改善、プロジェクトマネジメント）を提供しています。また、ご要望に合わせてWebシステムや業務システムの開発を行います。'
+        },
+        {
+          hid: 'service-og:url',
+          property: 'og:url',
+          content: 'https://eggsystem.co.jp/'
+        },
+        {
+          hid: 'service-og:image',
+          property: 'og:image',
+          content: 'https://eggsystem.co.jp/ogp/service-system01.JPG'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  letter-spacing: 1px;
+}
+.columns {
+  text-align: center;
+  position: relative;
+  margin: auto;
+  .column {
+    padding: 2rem;
+    margin-bottom: 3rem;
+  }
+}
+figure.image {
+  margin-bottom: 1rem;
+}
+.is-size-5 {
+  margin-bottom: 1rem;
+}
+@media screen and (min-width: 769px) {
+  .consulting::before {
+    position: absolute;
+    content: '';
+    width: 1px;
+    height: 80%;
+    top: 5%;
+    left: 50%;
+    background: $lightgray;
+  }
+}
+@media screen and (max-width: 768px) {
+  .consulting::after {
+    position: absolute;
+    content: '';
+    width: 90%;
+    height: 1px;
+    left: 5%;
+    background: $lightgray;
+    margin-top: 3rem;
+  }
+}
+.link {
+  width: auto;
+  margin-left: 2rem;
+  margin-right: 2rem;
+  text-align: center;
+}
+</style>
