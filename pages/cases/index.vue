85d466c0d6d76d4e7dd3f285085401ca3a65dc71
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-09-14 11:49:27 +0900

    feat : 導入事例のつなぎこみ

---
+++ pages/cases/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="page-content">
+      <the-hero-title main-text="顧客インタビュー・事例紹介" />
+      <the-news-list :news="listData"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheHeroTitle from '~/components/pages/common/TheHeroTitle.vue'
+import TheNewsList from '~/components/pages/common/TheNewsList.vue'
+import { fetchCmsListDataCase } from '~/lib/cms'
+
+export default {
+  components: {
+    TheHeroTitle,
+    TheNewsList
+  },
+  head() {
+    return {
+      title: '顧客インタビュー・事例紹介',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: '株式会社エッグシステム 顧客インタビュー・事例紹介'
+        },
+        {
+          hid: 'keywords',
+          name: 'keywords',
+          content:
+            '顧客インタビュー・事例紹介, eggsystem, 株式会社エッグシステム'
+        },
+        {
+          hid: 'twitter:card',
+          property: 'twitter:card',
+          content: 'summary'
+        },
+        { hid: 'og:type', property: 'og:type', content: 'article' },
+        {
+          hid: 'og:title',
+          property: 'og:title',
+          content: '顧客インタビュー・事例紹介'
+        },
+        {
+          hid: 'og:description',
+          property: 'og:description',
+          content: '株式会社エッグシステム 顧客インタビュー・事例紹介'
+        },
+        {
+          hid: 'og:url',
+          property: 'og:url',
+          content: 'https://eggsystem.co.jp/cases'
+        },
+        {
+          hid: 'og:image',
+          property: 'og:image',
+          content: 'https://eggsystem.co.jp/logo.png'
+        }
+      ]
+    }
+  },
+
+  asyncData() {
+    return fetchCmsListDataCase(23)
+  }
+}
+</script>
