e1cd40dec540021a60965a1e45996e67e4296796
Tsubasa <t.takahashi@eggsystem.co.jp>
2020-09-16 11:16:53 +0900

    トップページ改善
    ターゲットへの訴求ポイントを明確化して追加

--- components/pages/top/TheMainMessage.vue
+++ components/pages/top/TheMainMessage.vue
@@ -5,9 +5,82 @@
       そう言っていただける関係を目指しています
     </div>
     <p>
-      中小企業が抱える「経験豊富なIT人材の不足」「システムを有効活用できない」という課題を解決する<br>
-      コンサルティングからシステム開発まで一貫して行うコンサルティングエンジニア集団です。
+      わたしたちは100名以下のベンチャー・中小企業へ徹底的に寄り添う<br>
+      システムコンサルティング集団です。
     </p>
+    <div class="main-image-area">
+      <div class="main-image">
+        <div class="main-feature">
+          上場準備会社では内部統制への対応が必須<br>
+          ITシステム環境構築や規程類の作成には時間がかかる
+        </div>
+        <div>
+          <img :src="triangleImage" class="triangle-img" alt="解決策">
+          <div class="main-feature">
+            <div>
+              <br>
+              上場準備会社での内部統制支援実績あり<br>
+              <span class="font-red">監査をクリアし運用可能</span>なIT全般統制への対応へ
+            </div>
+            <nuxt-link to="" class="link case-button button is-dark">
+              <span class="link-message">上場準備会社の事例へ(準備中)</span>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+      <div class="main-image">
+        <div class="main-feature">
+          整体・エステ・パーソナルトレーニングジム等<br>
+          実店舗の売上を上げたい
+        </div>
+        <div>
+          <img :src="triangleImage" class="triangle-img" alt="解決策">
+          <div class="main-feature">
+            <div>
+              <br>
+              システムでオペレーションを自動化<br>
+              売上に直結する業務へ時間を割くことで<span class="font-red">売上拡大へ</span>
+            </div>
+            <nuxt-link to="/cases/olive-japan" class="link case-button button is-dark">
+              <span class="link-message">実店舗の売上拡大の事例へ</span>
+            </nuxt-link>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="main-image-area">
+      <div class="main-image">
+        <div class="main-feature">
+          ECサイトでの販売を始め<br>
+          Amazon/楽天/Yahoo等のチャネルを増やし売上を伸ばしたい
+        </div>
+        <div>
+          <img :src="triangleImage" class="triangle-img" alt="解決策">
+          <div class="main-feature">
+            <div>
+              <br>
+              煩雑な作業を最小限にする業務・システムを構築し<br>
+              <span class="font-red">手間を増やさずに売上拡大へ</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="main-image">
+        <div class="main-feature">
+          社員採用を進めたい<br><br>
+        </div>
+        <div>
+          <img :src="triangleImage" class="triangle-img" alt="解決策">
+          <div class="main-feature">
+            <div>
+              <br>
+              リモートワーク導入により働き方を改革し<br>
+              <span class="font-red">社員の満足度を上げ、採用のアピールへ</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <div class="issue-button" @click="isOpened = !isOpened">
       詳しく見る {{ isOpened ? '-' : '+' }}
     </div>
@@ -20,10 +93,16 @@
 
 <script>
 import TheIssues from './TheIssues.vue'
+import triangleImage from '~/assets/top/top-triangle.png'
 
 export default {
   components: { TheIssues },
-  data: () => ({ isOpened: false })
+  data: () => ({ isOpened: false }),
+  computed: {
+    triangleImage() {
+      return triangleImage
+    }
+  }
 }
 </script>
 
@@ -34,14 +113,20 @@ export default {
   text-align: center;
 
   .main-message {
-    font-size: 1.5rem;
+    font-size: 1.8rem;
     line-height: 3rem;
     margin-bottom: 1.5rem;
+    padding-top: 4%;
+    padding-bottom: 4%;
+    border-top: 1px solid $lightgray;
+    border-bottom: 1px solid $lightgray;
   }
 
   p {
-    line-height: 2rem;
-    margin-bottom: 2rem;
+    line-height: 3rem;
+    margin-top: 3rem;
+    margin-bottom: 3rem;
+    font-size: 1.5rem;
   }
 
   .issue-button {
@@ -54,9 +139,92 @@ export default {
     margin-top: 2.5rem;
   }
 
+  .case-button {
+    width: 65%;
+    height: 49px;
+    margin-top: 2.5rem;
+  }
+
+  .main-image-area {
+    display: flex;
+    justify-content: center;
+
+    .main-image {
+      width: 50%;
+      border: 1px solid #dbdbdb;
+      margin: 0 1rem 3rem;
+      padding: 1rem 2rem;
+
+      .triangle-img {
+        width: 20%;
+      }
+
+      .main-feature {
+        font-weight: bold;
+        margin-bottom: 2rem;
+
+        .font-red {
+          color: #f00;
+        }
+
+        .link-message {
+          font-weight: normal;
+        }
+      }
+
+      .main-details {
+        display: flex;
+      }
+
+      .free-image div {
+        width: 50%;
+      }
+
+      .contract-image div {
+        width: 33.3333%;
+      }
+
+      .main-notice {
+        margin-top: 1rem;
+        text-align: left;
+
+        span {
+          font-size: 0.5rem;
+        }
+      }
+    }
+  }
+
   @media screen and (max-width: 600px) {
     .service-button {
       width: 100%;
+    }
+    .case-button {
+      width: 100%;
+    }
+
+    .main-image-area {
+      display: flex;
+      justify-content: center;
+
+      .main-image {
+        width: 90%;
+      }
+    }
+
+    .main-image-area {
+      flex-flow: column;
+      align-items: center;
+    }
+
+    .main-image {
+      width: 100%;
+      margin-bottom: 3rem;
+    }
+
+    .reason-button {
+      margin: 1rem 0 1rem;
+      cursor: pointer;
     }
   }
 }
