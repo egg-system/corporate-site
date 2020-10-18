226b505a2bda0bb542aba3892a471032b8b60349
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-10-18 15:57:28 +0900

    fix : リンクの修正

--- components/pages/service/common/TheOtherServiceDetail.vue
+++ components/pages/service/common/TheOtherServiceDetail.vue
@@ -39,7 +39,7 @@
           </ul>
         </div>
         <div class="service-detail-btn">
-          <nuxt-link to="/service/consulting-engineer" class="case-button button is-dark">
+          <nuxt-link to="/service/others" class="case-button button is-dark">
             <span class="link-message">詳しく見る</span>
           </nuxt-link>
         </div>
