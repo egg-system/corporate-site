c3a9ae5dd7468f98f04ac4aebc865bff016920b3
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-10-27 18:13:46 +0900

    feat : spとpcで表示訳

---
+++ components/pages/service/consulting-engineer/TheServiceSpMenuTable.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="service-table-wrapper">
+    <table class="service-table">
+      <tr class="service-table-header">
+        <th colspan="2">
+          お試しサービス
+        </th>
+      </tr>
+      <tr class="service-table-content">
+        <td>料金</td>
+        <td>無料<br>※初月のみ</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>稼働時間</td>
+        <td>4日／月<br>（約32時間）</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>ご訪問</td>
+        <td>週1日</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>現地での作業<br>（常駐）</td>
+        <td>○</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>連絡手段</td>
+        <td>
+          Web会議（zoom）<br>
+          Chatwork<br>
+          slack
+        </td>
+      </tr>
+    </table>
+    <table class="service-table">
+      <tr class="service-table-header">
+        <th colspan="2">
+          アドバイザリーサービス
+        </th>
+      </tr>
+      <tr class="service-table-content">
+        <td>料金</td>
+        <td>5〜10万円</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>稼働時間</td>
+        <td>1〜2日／月<br>（約8〜20時間）</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>ご訪問</td>
+        <td>月1日</td>
+      </tr>
+
+      <tr class="service-table-content">
+        <td>料金</td>
+        <td>5万円</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>現地での作業<br>（常駐）</td>
+        <td>☓</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>連絡手段</td>
+        <td>
+          Web会議（zoom）<br>
+          Chatwork<br>
+          slack
+        </td>
+      </tr>
+    </table>
+    <table class="service-table">
+      <tr class="service-table-header">
+        <th colspan="2">
+          顧問サービス（コンサルティングエンジニアサービス）
+        </th>
+      </tr>
+      <tr class="service-table-content">
+        <td>稼働時間</td>
+        <td>
+          約8〜10日／月<br>（約64〜80時間）
+        </td>
+      </tr>
+      <tr class="service-table-content">
+        <td>ご訪問</td>
+        <td>週1〜2日</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>現地での作業<br>（常駐）</td>
+        <td>○</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>連絡手段</td>
+        <td>
+          Web会議（zoom）<br>
+          Chatwork<br>
+          slack<br>
+          またはお客様の社内ツール
+        </td>
+      </tr>
+    </table>
+    <table class="service-table">
+      <tr class="service-table-header">
+        <th colspan="2">
+          社員代行サービス（コンサルティングエンジニアサービス）
+        </th>
+      </tr>
+      <tr class="service-table-content">
+        <td>稼働時間</td>
+        <td>約15〜20日／月<br>（120〜160時間）</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>ご訪問</td>
+        <td>週3〜4日</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>現地での作業<br>（常駐）</td>
+        <td>○</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>連絡手段</td>
+        <td>
+          Web会議（zoom）<br>
+          Chatwork<br>
+          slack<br>
+          またはお客様の社内ツール
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '~/assets/service/scss/service-table.scss';
+</style>
