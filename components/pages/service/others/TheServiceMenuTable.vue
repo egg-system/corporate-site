b2ded8d7c6efc81b73a63fc1e8654a31dcbc50ef
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-10-27 18:53:12 +0900

    fix : ファイルリネーム

---
+++ components/pages/service/others/TheServiceMenuTable.vue
@@ -0,0 +1,16 @@
+<template>
+  <the-service-sp-menu-table v-if="$device.isMobile" />
+  <the-service-pc-menu-table v-else />
+</template>
+
+<script>
+import TheServicePcMenuTable from './TheServicePcMenuTable'
+import TheServiceSpMenuTable from './TheServiceSpMenuTable'
+
+export default {
+  components: {
+    TheServicePcMenuTable,
+    TheServiceSpMenuTable
+  }
+}
+</script>
