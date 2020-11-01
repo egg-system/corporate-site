3e1cb40926e22cfe9456d65c43d2d10215628b59
Tsubasa <t.takahashi@eggsystem.co.jp>
2020-11-01 16:48:34 +0900

    訴求する文章を見直し・変更

--- components/pages/top/TheMainMessage.vue
+++ components/pages/top/TheMainMessage.vue
@@ -8,41 +8,45 @@
       わたしたちは100名以下のベンチャー・中小企業へ徹底的に寄り添う<br>
       システムコンサルティング集団です。
     </h2>
+    <div class="sub-description">
+      ITシステムを活用し生産性を向上することで、人口減少・働き方改革など社会の変化へ対応します。<br class="pc-br">中小企業のDX（デジタルトランスフォーメーション）を推進し、生産性を向上します。
+    </div>
+    <br><br>
     <div class="main-image-area">
       <div class="main-image">
         <div class="main-feature">
-          資金調達後のベンチャー企業では内部統制への対応が必須だが<br>
-          ITシステム環境構築や規程書類の作成に時間がかかる
+          【資金調達後のベンチャー企業向け】<br>
+          内部統制（IT統制）への対応には手間がかかる<br class="pc-br">ITシステムの環境構築・評価、規程書類の作成を全て任せたい
         </div>
         <div>
           <img :src="triangleImage" class="triangle-img" alt="解決策">
           <div class="main-feature">
             <div>
               <br>
-              監査法人の指摘ゼロを達成した実績あり<br>
-              <span class="font-red">監査をクリアし運用可能</span>なIT統制環境を整備
+              監査法人の指摘ゼロの実績を元に<br>
+              <span class="font-red">安全面と運用面を考慮した</span>IT統制環境を整備
             </div>
             <nuxt-link to="/cases/innophys" class="link case-button button is-dark">
-              <span class="link-message">ベンチャー企業の内部統制事例へ</span>
+              <span class="link-message">ベンチャー企業の内部統制支援事例へ</span>
             </nuxt-link>
           </div>
         </div>
       </div>
       <div class="main-image">
         <div class="main-feature">
-          整体・エステ・パーソナルトレーニングジム等<br>
-          実店舗の売上を上げたい
+          【サービス業向け】<br class="pc-br">（整体・パーソナルトレーニングジム・エステなど）<br>
+          　人手不足を解消したい、店舗の売上を上げたい
         </div>
         <div>
           <img :src="triangleImage" class="triangle-img" alt="解決策">
           <div class="main-feature">
             <div>
               <br>
-              システムでオペレーションを自動化<br>
-              売上に直結する業務へ時間を割くことで<span class="font-red">売上拡大へ</span>
+              システムでオペレーションを自動化し<span class="font-red">人手不足を解消</span><br>
+              売上に直結するサービス提供へ時間を割くことで<span class="font-red">売上を拡大</span>
             </div>
             <nuxt-link to="/cases/olive-japan" class="link case-button button is-dark">
-              <span class="link-message">実店舗の売上拡大の事例へ</span>
+              <span class="link-message">整体店舗の売上拡大の事例へ</span>
             </nuxt-link>
           </div>
         </div>
@@ -51,32 +55,38 @@
     <div class="main-image-area">
       <div class="main-image">
         <div class="main-feature">
-          ECサイトでの販売を始め<br>
-          Amazon/楽天/Yahoo等のチャネルを増やし売上を伸ばしたい
+          【製造・卸売・小売業向け】<br class="pc-br">集客に苦戦するオンライン販売での売上を拡大したい<br>
+          余剰在庫を解消し適正な数の在庫を管理したい
         </div>
         <div>
           <img :src="triangleImage" class="triangle-img" alt="解決策">
           <div class="main-feature">
             <div>
               <br>
-              業務を改善して煩雑な作業を最小限にすることで<br>
-              <span class="font-red">手間を増やさずにチャネル増加・売上拡大へ</span>
+              システム同士を連携、煩雑な業務を自動化することで<br class="pc-br"><span class="font-red">手間を増やさずに販路拡大、在庫管理の適正化へ</span>
             </div>
+            <nuxt-link to="/service/consulting-engineer" class="link case-button button is-dark">
+              <span class="link-message">売上拡大・業務効率化の詳細へ</span>
+            </nuxt-link>
           </div>
         </div>
       </div>
       <div class="main-image">
         <div class="main-feature">
-          社員を採用したい<br><br>
+          【全業種向け】<br class="pc-br">
+          働き方改革やコロナ対策としてテレワークを本格導入したい<br class="pc-br">セキュリティを確保した仕組みと社員教育も行いたい<br>
         </div>
         <div>
           <img :src="triangleImage" class="triangle-img" alt="解決策">
           <div class="main-feature">
             <div>
               <br>
-              リモートワーク導入により働き方を改革し<br>
-              <span class="font-red">社員の満足度を上げ、採用のアピールへ</span>
+              セキュリティを担保したテレワークの仕組みの導入から<br class="pc-br">
+              社員への教育まで行い<span class="font-red">テレワークを社内で定着</span>
             </div>
+            <nuxt-link to="/service/others" class="link case-button button is-dark">
+              <span class="link-message">リモートワーク導入の詳細へ</span>
+            </nuxt-link>
           </div>
         </div>
       </div>
