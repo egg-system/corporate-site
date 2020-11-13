45e5a7dfba0326af3ed1d6aa9025e8b5379ba264
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-11-13 22:44:32 +0900

    feat : 資料ダウンロードページの追加

---
+++ components/pages/download/TheForm.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="google-form">
+    <iframe
+      class="form-frame"
+      src="https://docs.google.com/forms/d/e/1FAIpQLSd7bYgAoYMpjtoK8aPJJOrWFiGEOZoPm0I_8oBs8LxTsuB4Wg/viewform?embedded=true"
+      width="100%"
+      height="1000"
+      frameborder="0"
+      marginheight="0"
+      marginwidth="0"
+    >読み込んでいます…</iframe>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+iframe.form-frame {
+  height: 1800px;
+}
+@media screen and (max-width: 600px) {
+  iframe.form-frame {
+    height: 2200px;
+  }
+}
+</style>
---
+++ pages/download/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="content page-content">
+      <the-hero-title main-text="資料ダウンロード" />
+      <the-form />
+    </div>
+  </div>
+</template>
+
+<script>
+import TheHeroTitle from '~/components/pages/common/TheHeroTitle.vue'
+import TheForm from '~/components/pages/download/TheForm.vue'
+
+export default {
+  components: {
+    TheHeroTitle,
+    TheForm
+  },
+  head() {
+    return {
+      title: '資料ダウンロード',
+      meta: [
+        {
+          hid: 'contact-description',
+          name: 'description',
+          content: '株式会社エッグシステム 資料ダウンロード'
+        },
+        {
+          hid: 'contact-keywords',
+          name: 'keywords',
+          content: '資料ダウンロード, eggsystem, 株式会社エッグシステム'
+        },
+        {
+          hid: 'contact-twitter:card',
+          property: 'twitter:card',
+          content: 'summary'
+        },
+        { hid: 'contact-og:type', property: 'og:type', content: 'article' },
+        {
+          hid: 'contact-og:title',
+          property: 'og:title',
+          content: '資料ダウンロード'
+        },
+        {
+          hid: 'contact-og:description',
+          property: 'og:description',
+          content: '株式会社エッグシステム 資料ダウンロード'
+        },
+        {
+          hid: 'contact-og:url',
+          property: 'og:url',
+          content: 'https://eggsystem.co.jp/contact'
+        }
+      ]
+    }
+  }
+}
+</script>
