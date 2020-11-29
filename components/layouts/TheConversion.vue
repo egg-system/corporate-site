dcdd583137e316186cac712993b736292b19c5aa
nuno929 <t-mark5@kce.biglobe.ne.jp>
2020-11-29 18:38:12 +0900

    feat : 文言・デザインの調整

--- components/layouts/TheConversion.vue
+++ components/layouts/TheConversion.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="conversion">
     <nuxt-link to="/download">
-      資料ダウンロードはこちら
+      『IT投資を成功に導く方法』の<br>ダウンロードはこちら
     </nuxt-link>
   </div>  
 </template>
@@ -10,11 +10,23 @@
 .conversion {
   position: fixed;
   bottom: 5rem;
-  right: 3rem;
   background-color: #333;
   padding: 1rem 2rem;
   border: 1px solid #fff;
   border-radius: 4px;
+
+  @media screen and (max-width: 769px) {
+    width: 90%;
+    margin: 0 5%;
+  }
+
+  @media screen and (min-width: 770px) {
+    right: 3rem;
+
+    br {
+      display: none;
+    }
+  }
 
   a {
     color: whitesmoke;
