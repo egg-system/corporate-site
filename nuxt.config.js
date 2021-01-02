acc52fa71238ec4faefe78ff4631998ff611055a
nuno929 <t-mark5@kce.biglobe.ne.jp>
2021-01-02 18:34:48 +0900

    fix : hotJarに変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 import { routing } from './lib/cms'
 const { microCmsApiKey, microCmsApiDomain } = process.env
-const userHeatFunction = "(function(add, cla){window['UserHeatTag']=cla;window[cla]=window[cla]||function(){(window[cla].q=window[cla].q||[]).push(arguments)},window[cla].l=1*new Date();var ul=document.createElement('script');var tag = document.getElementsByTagName('script')[0];ul.async=1;ul.src=add;tag.parentNode.insertBefore(ul,tag);})('//uh.nakanohito.jp/uhj2/uh.js', '_uhtracker');_uhtracker({id:'uhOc9JVJnA'});"
+const hotJarFunction = "(function(h,o,t,j,a,r){h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};h._hjSettings={hjid:2178076,hjsv:6};a=o.getElementsByTagName('head')[0];r=o.createElement('script');r.async=1;r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;a.appendChild(r);})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');"
 
 export default {
   env: {
@@ -63,13 +63,13 @@ export default {
     ],
     script: [
       {
-        hid: 'userHeatFunction',
+        hid: 'hotJarFunction',
         type: 'text/javascript',
-        innerHTML: userHeatFunction
+        innerHTML: hotJarFunction
       }
     ],
     __dangerouslyDisableSanitizersByTagID: {
-      'userHeatFunction': ['innerHTML'],
+      'hotJarFunction': ['innerHTML'],
     }
   },
   /*
