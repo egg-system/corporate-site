2560c7fbee486fdead690aeea6e043c2b8c3e230
chiaki-w <c.watanabe@eggsystem.co.jp>
2022-05-30 11:42:03 +0900

    サービスメニュー・料金プラン（モバイル版）の修正

--- components/pages/service/it-control/TheServiceSpMenuTable.vue
+++ components/pages/service/it-control/TheServiceSpMenuTable.vue
@@ -2,63 +2,93 @@
   <div class="service-table-wrapper">
     <table class="service-table">
       <tr class="service-table-header">
-        <th colspan="2">お試しサービス</th>
+        <th colspan="2">ミニマムプラン</th>
       </tr>
       <tr class="service-table-content">
-        <td>料金</td>
-        <td>無料<br>※初月のみ</td>
+        <td>初期料金</td>
+        <td>100,000円</td>
       </tr>
       <tr class="service-table-content">
-        <td>対応期間</td>
+        <td>月額料金</td>
+        <td>0円</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>契約期間</td>
         <td>
-          1ヶ月<br>（約32時間）
+          なし<br>（買い切り）
         </td>
       </tr>
       <tr class="service-table-content">
-        <td>ご訪問</td>
-        <td>週1日</td>
+        <td>サポート内容</td>
+        <td>◎IT関連規程および手順書のフォーマットを提供</td>
       </tr>
       <tr class="service-table-content">
-        <td>現地での作業<br>（常駐）</td>
-        <td>○</td>
-      </tr>
-      <tr class="service-table-content">
-        <td>連絡手段</td>
-        <td>
-          Web会議（zoom）<br>
-          Chatwork<br>
-          slack
-        </td>
+        <td>専任サポート</td>
+        <td>なし</td>
       </tr>
     </table>
     <table class="service-table">
       <tr class="service-table-header">
-        <th colspan="2">内部統制支援サービス</th>
+        <th colspan="2">ベーシックプラン</th>
       </tr>
       <tr class="service-table-content">
-        <td>料金</td>
-        <td>60〜180万円</td>
+        <td>初期料金</td>
+        <td>0円</td>
       </tr>
       <tr class="service-table-content">
-        <td>対応期間</td>
+        <td>月額料金</td>
+        <td>300,000円</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>契約期間</td>
         <td>3ヶ月〜</td>
       </tr>
       <tr class="service-table-content">
-        <td>ご訪問</td>
-        <td>週1日</td>
+        <td>サポート内容</td>
+          ◎現状分析<br>
+          <br>
+          ◎文書作成<br>（IT関連規程、手順書）<br>
+          <br>
+          ◎全社的なIT統制/IT全般統制/IT業務処理統制の評価・整備<br>
+          <br>
+          ◎監査法人との協議支援<br>（質問書への回答作成）
       </tr>
       <tr class="service-table-content">
-        <td>現地での作業<br>（常駐）</td>
-        <td>○</td>
+        <td>専任サポート</td>
+        <td>あり</td>
+      </tr>
+    </table>
+    <table class="service-table">
+      <tr class="service-table-header">
+        <th colspan="2">ハイスペックプラン</th>
       </tr>
       <tr class="service-table-content">
-        <td>連絡手段</td>
-        <td>
-          Web会議（zoom）<br>
-          Chatwork<br>
-          slack<br>
-          またはお客様の社内ツール
-        </td>
+        <td>初期料金</td>
+        <td>0円</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>月額料金</td>
+        <td>600,000円</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>契約期間</td>
+        <td>1ヶ月〜</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>サポート内容</td>
+          ◎現状分析<br>
+          <br>
+          ◎文書作成<br>（IT関連規程、手順書、3点セット）<br>
+          <br>
+          ◎全社的なIT統制/IT全般統制/IT業務処理統制の評価・整備<br>
+          <br>
+          ◎監査法人との協議支援<br>（質問書への回答作成、打ち合わせ同席など）<br>
+          <br>
+          ◎規程で定めた運用ルールの策定支援・運用支援
+      </tr>
+      <tr class="service-table-content">
+        <td>専任サポート</td>
+        <td>あり</td>
       </tr>
     </table>
   </div>
