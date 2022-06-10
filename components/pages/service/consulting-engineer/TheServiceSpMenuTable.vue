02988af489c4bd3bbfe28ece9b4db94cbf7b3742
chiaki-w <c.watanabe@eggsystem.co.jp>
2022-06-10 16:10:48 +0900

    コンサルティングエンジニアサービスページの修正（スマホ版）

--- components/pages/service/consulting-engineer/TheServiceSpMenuTable.vue
+++ components/pages/service/consulting-engineer/TheServiceSpMenuTable.vue
@@ -1,131 +1,274 @@
 <template>
   <div class="service-table-wrapper">
+    <ul>
+      <li>各プランともに初期費用は発生しません。</li>
+      <li>貴社への常駐は行わず、リモートワーク（Web会議）を原則とさせていただきます。</li>
+      <li>ただし、必要に応じて貴社へのご訪問や対面でのお打ち合わせも実施いたします。</li>
+    </ul>
     <table class="service-table">
       <tr class="service-table-header">
-        <th colspan="2">
-          お試しサービス
+        <th>
+          ライトプラン
         </th>
       </tr>
       <tr class="service-table-content">
+        <td>概要</td>
+        <td>課題を1つずつ着実に対応します</td>
+      </tr>
+      <tr class="service-table-content">
         <td>料金</td>
-        <td>無料<br>※初月のみ</td>
+        <td>月額300,000円</td>
       </tr>
       <tr class="service-table-content">
-        <td>稼働時間</td>
-        <td>4日/月<br>（約32時間）</td>
+        <td>稼働時間<br>(目安)</td>
+        <td>4〜5人日/月<br>※隔週で定例会議を実施</td>
       </tr>
       <tr class="service-table-content">
-        <td>ご訪問</td>
-        <td>週1日</td>
+        <td>契約期間</td>
+        <td>3ヶ月単位</td>
       </tr>
       <tr class="service-table-content">
-        <td>現地での作業<br>（常駐）</td>
-        <td>○</td>
-      </tr>
-      <tr class="service-table-content">
-        <td>連絡手段</td>
-        <td>
-          Web会議（zoom）<br>
-          Chatwork<br>
-          slack
-        </td>
+        <td>弊社体制</td>
+        <td>1〜2名体制</td>
       </tr>
     </table>
     <table class="service-table">
       <tr class="service-table-header">
-        <th colspan="2">
-          アドバイザリーサービス
+        <th>
+          スタンダードプラン
         </th>
       </tr>
       <tr class="service-table-content">
+        <td>概要</td>
+        <td>業務改善に限らずシステム全体やネットワーク、セキュリティなど幅広い課題へ対応します</td>
+      </tr>
+      <tr class="service-table-content">
         <td>料金</td>
-        <td>5万円</td>
+        <td>月額600,000円</td>
       </tr>
       <tr class="service-table-content">
-        <td>稼働時間</td>
-        <td>1日/月<br>（約10時間）</td>
+        <td>稼働時間<br>(目安)</td>
+        <td>約10人日/月<br>※週1回の定例会議を実施</td>
       </tr>
       <tr class="service-table-content">
-        <td>ご訪問</td>
-        <td>月1日</td>
+        <td>契約期間</td>
+        <td>1ヶ月単位</td>
       </tr>
       <tr class="service-table-content">
-        <td>現地での作業<br>（常駐）</td>
-        <td>☓</td>
-      </tr>
-      <tr class="service-table-content">
-        <td>連絡手段</td>
-        <td>
-          Web会議（zoom）<br>
-          Chatwork<br>
-          slack
-        </td>
+        <td>弊社体制</td>
+        <td>2名体制</td>
       </tr>
     </table>
     <table class="service-table">
       <tr class="service-table-header">
-        <th colspan="2">
-          顧問サービス（コンサルティングエンジニアサービス）
+        <th>
+          プレミアムプラン
         </th>
       </tr>
       <tr class="service-table-content">
+        <td>概要</td>
+        <td>貴社の社員代行として現場へ入り込んでリソースを補完します</td>
+      </tr>
+      <tr class="service-table-content">
         <td>料金</td>
-        <td>50〜60万円</td>
+        <td>※都度お見積り</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>稼働時間<br>(目安)</td>
+        <td>約10〜20人日/月</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>契約期間</td>
+        <td>1ヶ月単位</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>弊社体制</td>
+        <td>2〜3名体制</td>
+      </tr>
+    </table>
+    <br>
+    <br>
+    <p>
+      【凡例】◯：対応、△：実作業を除くアドバイスのみ対応、✕：非対応
+    </p>
+    <table class="service-table">
+      <tr class="service-table-header">
+        <th>
+          ライトプラン
+        </th>
       </tr>
       <tr class="service-table-content">
         <td>稼働時間</td>
-        <td>
-          4〜5日/月<br>（約32〜50時間）
-        </td>
+        <td>約40時間／月</td>
       </tr>
       <tr class="service-table-content">
-        <td>ご訪問</td>
-        <td>週1〜2日</td>
+        <td colspan="2">サポート範囲</td>
       </tr>
       <tr class="service-table-content">
-        <td>現地での作業<br>（常駐）</td>
-        <td>○</td>
+        <td>現状分析</td>
+        <td>◯</td>
       </tr>
       <tr class="service-table-content">
-        <td>連絡手段</td>
-        <td>
-          Web会議（zoom）<br>
-          Chatwork<br>
-          slack<br>
-          またはお客様の社内ツール
-        </td>
+        <td>業務設計</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>システム選定</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>システム導入</td>
+        <td>△</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>開発作業</td>
+        <td>✕</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>運用支援</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td colspan="2">対象領域</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>業務改善</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>インフラ</td>
+        <td>△</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>ネットワーク</td>
+        <td>△</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>セキュリティ</td>
+        <td>△</td>
+      </tr>
+    </table>
+    <br>
+    <br>
+    <div class="manu-compare">
+      メニュー毎の比較
+    </div>
+    <br>
+    <p>
+      【凡例】◯：対応、△：実作業を除くアドバイスのみ対応、✕：非対応
+    </p>
+    <table class="service-table">
+      <tr class="service-table-header">
+        <th>
+          スタンダードプラン
+        </th>
+      </tr>
+      <tr class="service-table-content">
+        <td>稼働時間</td>
+        <td>約80時間／月</td>
+      </tr>
+      <tr class="service-table-content">
+        <td colspan="2">サポート範囲</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>現状分析</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>業務設計</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>システム選定</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>システム導入</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>開発作業</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>運用支援</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td colspan="2">対象領域</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>業務改善</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>インフラ</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>ネットワーク</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>セキュリティ</td>
+        <td>◯</td>
       </tr>
     </table>
     <table class="service-table">
       <tr class="service-table-header">
-        <th colspan="2">
-          社員代行サービス（コンサルティングエンジニアサービス）
+        <th>
+          プレミアムプラン
         </th>
       </tr>
       <tr class="service-table-content">
-        <td>料金</td>
-        <td>100〜150万円</td>
-      </tr>
-      <tr class="service-table-content">
         <td>稼働時間</td>
-        <td>10〜15日/月<br>（80〜150時間）</td>
+        <td>約80時間〜／月</td>
       </tr>
       <tr class="service-table-content">
-        <td>ご訪問</td>
-        <td>週3〜4日</td>
+        <td colspan="2">サポート範囲</td>
       </tr>
       <tr class="service-table-content">
-        <td>現地での作業<br>（常駐）</td>
-        <td>○</td>
+        <td>現状分析</td>
+        <td>◯</td>
       </tr>
       <tr class="service-table-content">
-        <td>連絡手段</td>
-        <td>
-          Web会議（zoom）<br>
-          Chatwork<br>
-          slack<br>
-          またはお客様の社内ツール
-        </td>
+        <td>業務設計</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>システム選定</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>システム導入</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>開発作業</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>運用支援</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td colspan="2">対象領域</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>業務改善</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>インフラ</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>ネットワーク</td>
+        <td>◯</td>
+      </tr>
+      <tr class="service-table-content">
+        <td>セキュリティ</td>
+        <td>◯</td>
       </tr>
     </table>
   </div>
