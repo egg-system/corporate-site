b39a3aacc415ae716f434b9f6bb6ae136c8ae833
Tsubasa <38577320+eggsystem@users.noreply.github.com>
2023-05-15 23:05:30 +0900

    Create gtag.vue

---
+++ components/gtag.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'gtag',
+  props: {},
+  mounted() {
+    const script = document.createElement('script');
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-9LX6NE1NH7';
+    script.async = true;
+    document.head.appendChild(script);
+  },
+};
+</script>
+
+<style>
+</style>
