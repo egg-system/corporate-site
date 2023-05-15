47664c62f357984136234fb67c9325506cc7df8b
Tsubasa <38577320+eggsystem@users.noreply.github.com>
2023-05-15 23:10:56 +0900

    Create gtag.js

---
+++ assets/js/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-9LX6NE1NH7"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9LX6NE1NH7');
+</script>
