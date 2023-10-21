c80a7b956aabc3bece21cbbbc76901bb5804d760
Tsubasa <38577320+eggsystem@users.noreply.github.com>
2023-10-22 00:01:53 +0900

    Update TheServiceSpMenuTable.vue

--- components/pages/service/it-control/TheServiceSpMenuTable.vue
+++ components/pages/service/it-control/TheServiceSpMenuTable.vue
@@ -37,7 +37,7 @@
       </tr>
       <tr class="service-table-content">
         <td>月額料金</td>
-        <td>500,000円</td>
+        <td>600,000円</td>
       </tr>
       <tr class="service-table-content">
         <td>契約期間</td>
